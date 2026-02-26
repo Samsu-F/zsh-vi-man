c9be51915cab6c325b377af1e70f9916f7c78345
Samsu-F <samsu-f@web.de>
2026-02-26 19:07:51 +0100

    hide stderr of man when checking if matches exist

--- lib/pager.zsh
+++ lib/pager.zsh
@@ -84,7 +84,7 @@ zvm_open_less() {
   
   # Always override system MANPAGER
   # This prevents issues when MANPAGER is set to nvim/vim but ZVM_MAN_PAGER is less
-  if [[ -n "$pattern" ]] && man --pager=cat "$man_page" | grep -qE "${pattern}"; then
+  if [[ -n "$pattern" ]] && man --pager=cat "$man_page" 2>/dev/null | grep -qE "${pattern}"; then
     man --pager="${ZVM_MAN_PAGER} -p '${pattern}'" "$man_page" || return 1
   else
     man --pager="${ZVM_MAN_PAGER}" "$man_page" || return 1
